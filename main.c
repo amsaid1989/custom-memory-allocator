1442be32bbbed703512a24a0c3b5c01caaa7c7ce
Abdelrahman Said <said.abdelrahman89@gmail.com>
2023-05-16 00:10:20 +0100

    Add main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "aliases.h"
+#include "allocator.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 10 * 1024 * 1024
+
+i32 main(i32 argc, char *argv[]) {
+  allocator_t *alloc = init_allocator(BUF_SIZE);
+
+  if (!alloc) {
+    return EXIT_FAILURE;
+  }
+
+  deinit_allocator(&alloc);
+
+  return EXIT_SUCCESS;
+}
